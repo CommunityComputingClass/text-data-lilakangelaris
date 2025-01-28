e066a137734504fdb16c3a726799ed36051418dd
lilakangelaris <125599089+lilakangelaris@users.noreply.github.com>
2025-01-28 23:33:12 +0000

    text worksheet

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,8 +1,23 @@
-function setup() {
-  createCanvas(400, 400);
+let myMessage = "mermaids"
+let xLocation;
+let yLocation;
+let myFont;
+
+function preload(){
+  myFont = loadFont('MercyChristole.ttf')
 }
 
+function setup() {
+  createCanvas(400, 400);
+
+}
 function draw() {
-  background(200);
-  
+  background(82, 255, 212);
+  noStroke();
+  fill(255, 161, 242);
+  textSize(50)
+  textFont(myFont)
+  text(myMessage, xLocation, yLocation, 240);
+  xLocation = mouseX
+  yLocation = mouseY
 }
